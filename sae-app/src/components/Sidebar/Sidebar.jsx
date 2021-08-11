5bdad3e7e3119f7cf4e2282a1944687157762627
Lucas-COX <lucas.cox@epitech.eu>
2021-08-11 16:50:09 +0200

    feat(sae-app): add routing

--- sae-app/src/components/Sidebar/Sidebar.jsx
+++ sae-app/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,12 @@
 import "./sidebar.css"
 import {Face, Adb, Home} from "@material-ui/icons"
 import Logo from "../Logo/Logo"
+import { Link } from "react-router-dom"
+import { useLocation } from "react-router"
+import clsx from "clsx"
 
 export default function Sidebar() {
+    const location = useLocation();
     return (
         <div className="sidebar">
             <div className="sidebarLogo">
@@ -12,18 +16,24 @@ export default function Sidebar() {
                 <div className="sidebarMenu">
                     <h3 className="sidebarTitle">Dashboard</h3>
                     <ul className="sidebarList">
-                        <li className="sidebarListItem">
-                            <Home className="sidebarIcon"/>
-                            Home
-                        </li>
-                        <li className="sidebarListItem">
-                            <Face className="sidebarIcon"/>
-                            Utilisateurs
-                        </li>
-                        <li className="sidebarListItem">
-                            <Adb className="sidebarIcon"/>
-                            Commandes
-                        </li>
+                        <Link to="/" className="sidebarListItemLink">
+                            <li className={clsx("sidebarListItem", location.pathname === "/" ? "active" : "")}>
+                                <Home className="sidebarIcon"/>
+                                Home
+                            </li>
+                        </Link>
+                        <Link to="/users" className="sidebarListItemLink">
+                            <li className={clsx("sidebarListItem", location.pathname === "users/" ? "active" : "")}>
+                                <Face className="sidebarIcon"/>
+                                Utilisateurs
+                            </li>
+                        </Link>
+                        <Link to="/commands" className="sidebarListItemLink">
+                            <li className={clsx("sidebarListItem", location.pathname === "/commands" ? "active" : "")}>
+                                <Adb className="sidebarIcon"/>
+                                Commandes
+                            </li>
+                        </Link>
                     </ul>
                 </div>
             </div>
