4294bd754c3f259e1aeeaa97566d5802d75e1a5e
Lucas-COX <lucas.cox@epitech.eu>
2021-08-17 15:21:46 +0200

    chore(sae-app): remove useless console.log

--- sae-app/src/utils.js
+++ sae-app/src/utils.js
@@ -5,8 +5,6 @@ export function getPage(path) {
     if (!path) return null;
     let newPath = toString(path);
     newPath = path.substring(0, path.length - 4);
-    console.log(pages);
-    console.log(newPath);
     return pages[newPath];
 }
 
