9e7d8a767dcfa894581fa9aaa3df1e3d49bb7828
Lucas-COX <lucas.cox@epitech.eu>
2021-08-18 22:41:56 +0200

    feat(sae-bot): add database creator and sets it as global variable

---
+++ sae-bot/scripts/create_database.sh
@@ -0,0 +1,17 @@
+printf "=======================================================\n"
+printf "|                                                     |\n"
+printf "| \033[0;33mCreating MySQL database for SaeBot...\033[0m               |\n"
+printf "| \033[0;34mMake sure that you have MySQL or MariaDB installed.\033[0m |\n"
+printf "|                                                     |\n"
+printf "=======================================================\n"
+
+tmp=$(which mysql)
+if [ $? != 0 ]
+then
+    printf $?
+    exit 1
+fi
+
+sudo mysql -e "CREATE USER IF NOT EXISTS 'saejina'@'localhost' IDENTIFIED BY 'SaejinaDBPWD:)';"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS saejinaDB;"
+sudo mysql -e "GRANT ALL PRIVILEGES ON saejinaDB.* TO 'saejina'@'localhost';"
