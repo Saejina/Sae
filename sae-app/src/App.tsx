6e232abc44c1bc7935b9208de5625129766d9a5a
Lucas-COX <lucas.cox@epitech.eu>
2021-08-19 17:26:45 +0200

    feat(sa-app): add notfound page

--- sae-app/src/App.tsx
+++ sae-app/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import routes from './configs/routes';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { NotFound } from './pages';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App(): JSX.Element {
     return (
         <Router>
             <Switch>
                 {routes.map(({ path, page }, index) => {
-                    return <Route key={index} path={path} component={page} />;
+                    return <Route exact key={index} path={path} component={page} />;
                 })}
+                <Redirect to="/404" />
             </Switch>
         </Router>
     );
--- sae-app/src/pages/index.ts
+++ sae-app/src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from './Commands';
 export * from './Home';
+export * from './NotFound';
 export * from './Users';
