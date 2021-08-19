f464d426d6bcdcea108244284400c112d9f6aa2d
Havok <72014202+Lucas-COX@users.noreply.github.com>
2021-08-20 00:42:06 +0200

    feat(sae-app): add color theme to bootstrap and tailwind (#5)

--- sae-app/tailwind.config.js
+++ sae-app/tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      'primary': '#f180f8',
+      'secondary': '#1f51ff',
+      'success': '#bc2cbc',
+      'warning': '#1f51ff',
+      'danger': '#54254e',
+      'light': '#d9c4d1',
+      'dark': '#393b63',
+      'info': '#b026ff'
+    }
   },
   variants: {
     extend: {},
