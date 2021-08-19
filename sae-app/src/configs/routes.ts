d7051b785d2b1bee75bcb68821fd32c3dd2c0081
Lucas-COX <lucas.cox@epitech.eu>
2021-08-19 17:26:45 +0200

    feat(sa-app): add notfound page

--- sae-app/src/configs/routes.ts
+++ sae-app/src/configs/routes.ts
@@ -3,6 +3,7 @@ import * as pages from '../pages';
 const routes = [
     { path: '/users', page: pages['Users'] },
     { path: '/commands', page: pages['Commands'] },
+    { path: '/404', page: pages['NotFound'] },
     { path: '/', page: pages['Home'] },
 ];
 
