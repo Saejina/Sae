2a357c5dfa93b6c246e86dbf13be5df0f9129168
Lucas-COX <lucas.cox@epitech.eu>
2021-08-19 17:26:45 +0200

    feat(sa-app): add notfound page

---
+++ sae-app/src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function NotFound(): JSX.Element {
+    return (
+        <div className={'flex items-center justify-center text-9xl'}>
+            <span>NOT FOUND</span>
+        </div>
+    );
+}
+
+export default NotFound;
