42c30ffac704c9b46e70d85b24d404bfc6337970
Lucas-COX <lucas.cox@epitech.eu>
2021-08-19 16:11:26 +0200

    feat(sae-app): change for typescript

---
+++ sae-app/src/configs/routes.ts
@@ -0,0 +1,9 @@
+import * as pages from '../pages';
+
+const routes = [
+    { path: '/users', page: pages['Users'] },
+    { path: '/commands', page: pages['Commands'] },
+    { path: '/', page: pages['Home'] },
+];
+
+export default routes;
