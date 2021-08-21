d547e141e7e0a829eda2263f39865e5bc29cc726
Havok <72014202+Lucas-COX@users.noreply.github.com>
2021-08-21 14:50:53 +0200

    feat(sae-app): create app layout (#6)
    * test
    
    * feat(sae-app): add app layout
    
    Co-authored-by: MarvinFlm <marvin.flamand@epitech.eu>

--- sae-app/tailwind.config.js
+++ sae-app/tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1/30': '3.5%',
+      }
+    },
     colors: {
       'primary': '#f180f8',
       'secondary': '#1f51ff',
@@ -11,7 +15,8 @@ module.exports = {
       'danger': '#54254e',
       'light': '#d9c4d1',
       'dark': '#393b63',
-      'info': '#b026ff'
+      'info': '#b026ff',
+      'darker': '#31334d'
     }
   },
   variants: {
