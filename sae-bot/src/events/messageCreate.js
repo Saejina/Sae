628a0b7fee6cd7d9c9cea34d42187467bb992616
Lucas-COX <lucas.cox@epitech.eu>
2021-09-15 15:32:32 +0200

    feat(sae-bot): add help command

--- sae-bot/src/events/messageCreate.js
+++ sae-bot/src/events/messageCreate.js
@@ -8,11 +8,11 @@ module.exports = (client, message) => {
     if (message.content.startsWith(global.prefix)) {
         const content = message.content.split(' ')[0].slice(global.prefix.length).toLowerCase();
         if (global.CommandList.has(global.prefix + content)) {
-            if (!message.member.permissions.has(global.CommandList.get(global.prefix + content).c.permissions)) {
+            if (!message.member.permissions.has(global.CommandList.get(global.prefix + content).permissions)) {
                 setTimeout(() => message.delete(), 3000);
                 message.reply("Tu n'as pas les permissions nécessaires pour utiliser cette commande.").then((msg) => setTimeout(() => msg.delete(), 3000));
             } else {
-                global.CommandList.get(global.prefix + content).c.action(client, message);
+                global.CommandList.get(global.prefix + content).action(client, message);
             }
         } else {
             const CMDLIST = [];
