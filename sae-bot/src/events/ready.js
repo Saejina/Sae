89db5cf2b41a183df06b03dd46e1a766afd55d13
Havok <72014202+Lucas-COX@users.noreply.github.com>
2021-10-03 01:16:38 +0200

    feat(sae-bot): remove useless tickets and reactionroles for hosting (#25)

--- sae-bot/src/events/ready.js
+++ sae-bot/src/events/ready.js
@@ -2,25 +2,30 @@ const requireAll = require('require-all');
 const path = require('path');
 const reload = require('../functions/reloadCMD');
 const manip = require('../utils/jsonManip');
-
-const files = requireAll({
-    dirname: `${__dirname}/../data/reactionroles`,
-    filter: /^(?!-)(.+)\.json$/,
-});
+const fs = require('fs');
 
 async function addMessagesToCache(client) {
+    const files = fs.existsSync(`${__dirname}/../data/reactionroles`) ? requireAll({
+        dirname: `${__dirname}/../data/reactionroles`,
+        filter: /^(?!-)(.+)\.json$/,
+    }) : [];
     const messages = Object.keys(files);
-    const tickets = await manip.readJsonFile(path.join(__dirname, '..', 'data/reactionTickets.json'));
+    const tickets = fs.existsSync() ? await manip.readJsonFile(path.join(__dirname, '..', 'data/reactionTickets.json'))
+    .catch((err) => {
+        console.log(`[SAE-BOT][ERROR] ${err}`);
+    }) : null;
     messages.forEach((mess) => {
         client.channels.fetch(files[mess].channel).then((channel) => {
             channel.messages.fetch(files[mess]);
         });
     });
-    tickets.forEach((ticket) => {
-        client.channels.fetch(ticket.channel).then((channel) => {
-            channel.messages.fetch(ticket.messages);
+    if (tickets && tickets.length > 0) {
+        tickets.forEach((ticket) => {
+            client.channels.fetch(ticket.channel).then((channel) => {
+                channel.messages.fetch(ticket.messages);
+            });
         });
-    });
+    }
 }
 
 module.exports = (client) => {
