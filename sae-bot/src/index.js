5756de561feae38bc36a99e46d0fafdb1f71ba90
Lucas-COX <lucas.cox@epitech.eu>
2021-10-06 09:44:39 +0200

    fix(sae-bot): stop database and suspend register command

--- sae-bot/src/index.js
+++ sae-bot/src/index.js
@@ -1,6 +1,6 @@
 const path = require('path');
 require('dotenv').config({ path: path.resolve(`${__dirname}../../.env`) });
-require('./configs/database');
+// require('./configs/database');
 const Discord = require('discord.js');
 const fs = require('fs');
 
